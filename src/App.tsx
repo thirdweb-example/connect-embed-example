4ba2a080b4f9339f3b357c5a498f8e70b088ddcb
Kien Ngo <github@kien.ngo>
2024-05-20 12:28:48 -0400

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import { createThirdwebClient } from "thirdweb";
 import "./App.css";
-import { ConnectEmbed } from "thirdweb/react";
+import { ConnectButton, ConnectEmbed, useActiveAccount } from "thirdweb/react";
 
 const client = createThirdwebClient({
   // If not using Vite, then use `process.env.NEXT_PUBLIC_CLIENT_ID`
   clientId: import.meta.env.VITE_CLIENT_ID,
 });
 function App() {
+  const account = useActiveAccount();
   return (
     <div>
-      <ConnectEmbed client={client} />
+      {account ? <ConnectButton client={client} /> : <ConnectEmbed client={client} />}
     </div>
   );
 }
